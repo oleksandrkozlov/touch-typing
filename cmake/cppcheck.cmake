3274f8c1b5cfb96ff971943bf4015029ef58df59
Oleksandr Kozlov <aleks.a.kozlov@gmail.com>
2020-04-19 17:03:38 +0200

    build: Refactor CMake modules
    Signed-off-by: Oleksandr Kozlov <aleks.a.kozlov@gmail.com>

--- cmake/cppcheck.cmake
+++ cmake/cppcheck.cmake
@@ -1,13 +1,13 @@
-find_program (CPPCHECK_PATH cppcheck)
+find_program(CPPCHECK_PROGRAM cppcheck)
 
-if (NOT CPPCHECK_PATH)
-    message (FATAL_ERROR "No program 'cppcheck' found")
-endif ()
+if(NOT CPPCHECK_PROGRAM)
+    message(FATAL_ERROR "No program 'cppcheck' found")
+endif()
 
-add_custom_target (
+add_custom_target(
     cppcheck
     COMMAND
-        ${CPPCHECK_PATH} --quiet --enable=all --project=${CMAKE_BINARY_DIR}/compile_commands.json
+        ${CPPCHECK_PROGRAM} --quiet --enable=all --project=${CMAKE_BINARY_DIR}/compile_commands.json
         --xml --xml-version=2 --suppress=unusedPrivateFunction --suppress=missingInclude
         --suppress=syntaxError --suppress=unmatchedSuppression --inline-suppr -i ${CMAKE_BINARY_DIR}
         ${CMAKE_SOURCE_DIR} 2> ${CMAKE_BINARY_DIR}/cppcheck.xml
