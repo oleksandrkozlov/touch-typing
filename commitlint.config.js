b1f4e38f058ab967e0b7a9f6d64916123ea2749c
Oleksandr Kozlov <aleks.a.kozlov@gmail.com>
2020-04-20 04:16:31 +0200

    ci: Add commitlint
    Signed-off-by: Oleksandr Kozlov <aleks.a.kozlov@gmail.com>

---
+++ commitlint.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    parserPreset: 'conventional-changelog-conventionalcommits',
+    rules: {
+        'body-leading-blank': [2, 'always'],
+        'body-max-line-length': [2, 'always', 72],
+        'footer-leading-blank': [2, 'always'],
+        'footer-max-line-length': [2, 'always', 72],
+        'header-max-length': [2, 'always', 50],
+        'scope-case': [2, 'always', 'lower-case'],
+        'subject-case': [
+            2,
+            'always',
+            ['sentence-case']
+        ],
+        'subject-empty': [2, 'never'],
+        'subject-full-stop': [2, 'never', '.'],
+        'type-case': [2, 'always', 'lower-case'],
+        'type-empty': [2, 'never'],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'fix',
+                'feat',
+                'chore',
+                'docs',
+                'style',
+                'refactor',
+                'perf',
+                'test',
+                'improv',
+                'build',
+                'ci',
+                'revert',
+            ]
+        ],
+        'signed-off-by': [2, 'always', 'Signed-off-by:']
+    }
+};
