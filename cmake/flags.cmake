065654609a41c3660270d46e8ea5acb3df1bc9c7
Oleksandr Kozlov <aleks.a.kozlov@gmail.com>
2020-10-18 22:01:34 +0200

    build: Add GNU compile options
    Signed-off-by: Oleksandr Kozlov <aleks.a.kozlov@gmail.com>

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -3,15 +3,17 @@ set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_VISIBILITY_PRESET hidden)
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-set(CMAKE_VISIBILITY_INLINES_HIDDEN 1)
+set(CMAKE_VISIBILITY_INLINES_HIDDEN ON)
 
 add_compile_options(
+    "$<$<CXX_COMPILER_ID:GNU>:-Wduplicated-branches;-Wduplicated-cond;-Wlogical-op;-Wuseless-cast>"
     -Wall
     -Wcast-align
+    -Wconversion
     -Wdouble-promotion
     -Wextra
-    -Wfloat-conversion
-    -Wformat
+    -Wformat=2
+    -Wmisleading-indentation
     -Wnon-virtual-dtor
     -Wnull-dereference
     -Wold-style-cast
