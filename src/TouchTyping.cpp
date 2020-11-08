1a0e3d84ff8acab7e9e5aae863953d28be51aff4
Oleksandr Kozlov <aleks.a.kozlov@gmail.com>
2020-11-08 07:21:30 +0100

    feat: Integrate docopt
    Signed-off-by: Oleksandr Kozlov <aleks.a.kozlov@gmail.com>

--- src/TouchTyping.cpp
+++ src/TouchTyping.cpp
@@ -5,11 +5,31 @@
 #include "Output.hpp"
 #include "TextStorage.hpp"
 
+#include <docopt/docopt.h>
+
 #include <memory>
 #include <utility>
 
-auto main() -> int
+namespace {
+
+constexpr auto usage = R"(
+Usage:
+    touch_typing (-f | --file) <file>
+
+Options:
+    -f, --file   Input file.
+)";
+
+} // namespace
+
+auto main(int argc, char** argv) -> int
 {
+    const auto arguments =
+        docopt::docopt(usage, {std::next(argv), std::next(argv, argc)});
+
+    [[maybe_unused]] const auto textToType =
+        arguments.at("<file>").asString();
+
     auto presenter = std::unique_ptr<touch_typing::Output>{
         std::make_unique<touch_typing::CursesPresenter>()};
 
