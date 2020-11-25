f4f9fe3e2f2116dacac1ea0e83da1b951a724ec7
Oleksandr Kozlov <aleks.a.kozlov@gmail.com>
2020-11-26 00:40:51 +0100

    build: Print CMake options
    Signed-off-by: Oleksandr Kozlov <aleks.a.kozlov@gmail.com>

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,2 +1,7 @@
-option(RUN_IWYU_ON_BUILD "Run 'iwyu' during the build" OFF)
-option(RUN_CLANG_TIDY_ON_BUILD "Run 'clang-tidy' during the build" OFF)
+function(set_option name description value)
+    option(name description value)
+    message(STATUS "${description}: ${value}")
+endfunction()
+
+set_option(RUN_IWYU_ON_BUILD "Run 'iwyu' during the build" OFF)
+set_option(RUN_CLANG_TIDY_ON_BUILD "Run 'clang-tidy' during the build" OFF)
